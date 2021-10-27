336bc35b97c1230d0c56dbb28c17fa7b83ab37d4
David Almonacid <david.almonacidf@gmail.com>
2021-10-26 20:29:07 -0500

    Card content improved. Trends and originals sections added.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const cards = document.querySelectorAll('.carousel__card.card')
-
+const cards = document.querySelectorAll('.card')
 cards.forEach((card, index) => {
-  card.style.backgroundImage = `
-    linear-gradient(rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.7)),
-    url(https://picsum.photos/175/225?random=${index + 1})
-  `
+  card.style.backgroundImage = `url(https://picsum.photos/175/225?random=${index})`
+})
+
+const cardsContent = document.querySelectorAll('.card__content')
+cardsContent.forEach((cardContent) => {
+  const playIcon = cardContent.appendChild(new Image())
+  playIcon.src = 'https://i.postimg.cc/C5FdFsmV/play.png'
+  playIcon.alt = 'Play icon'
+
+  if (!cardContent.parentElement.className.includes('myListCard')) {
+    const plusIcon = cardContent.appendChild(new Image())
+    plusIcon.src = 'https://i.postimg.cc/D0B0m4M4/plus.png'
+    plusIcon.alt = 'Plus icon'
+  }
+
+  const title = cardContent.appendChild(document.createElement('p'))
+  title.className = 'card__title'
+  title.textContent = 'Título'
+
+  const data = cardContent.appendChild(document.createElement('div'))
+  data.className = 'card__data'
+
+  const year = data.appendChild(document.createElement('p'))
+  year.textContent = 2021
+
+  const rating = data.appendChild(document.createElement('p'))
+  rating.textContent = '⭐8.5'
 })
